8a635bc5ed432d61a92cb124af47e4702c69cd21
Ihor Klymchuk <klymchuk.i@overdose.digital>
2023-06-15 13:59:07 +0300

    Finished basic download functionality

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -2,63 +2,49 @@
 
 namespace App\Controller;
 
+use App\Exception\EncryptionException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\File;
-use App\ServiceApi\Actions\GetDumpByUuid;
+use App\Service\DumpManagement;
+use App\Service\Encryption;
 
 class DownloadController extends AbstractController
 {
 
     public function __construct(
-        private readonly GetDumpByUuid $dumpService
+        private readonly DumpManagement $dumpService,
+        private readonly Encryption $encryption
     ) {
     }
 
     #[Route('/download', name: 'app_download')]
-    public function index(): JsonResponse | BinaryFileResponse
+    public function index(Request $request): JsonResponse | BinaryFileResponse
     {
-        $encryptedData = file_get_contents('php://input');
-        $keys = explode("\n", file_get_contents('/app/config/keys'));
+        $encryptedData = $request->getContent();
 
-        $decrypted = false;
-        foreach ($keys as $key) {
-            if (empty($key)) continue;
-            $key = "-----BEGIN PRIVATE KEY-----\n" . $key . "\n-----END PRIVATE KEY-----";
-            if (openssl_private_decrypt($encryptedData, $decryptedData, trim($key))) {
-                $decrypted = true;
-            }
-        }
-
-        if (!$decrypted) {
+        try {
+            $decryptedData = $this->encryption->decrypt($encryptedData);
+        } catch (EncryptionException $exception) {
             return $this->json([
                 'message' => 'Access denied'
             ], 503);
-        }
-
-        $decryptedData = json_decode($decryptedData, true);
-        $dumpService = $this->dumpService->execute($decryptedData['dumpuuid']);
-
-        if ($dumpService && !empty($dumpService['filename'])) {
-
-            $file = new File('/app/dumps/processed/' . $decryptedData['dbuuid'] . '/' . $dumpService['filename']);
-            return $this->file($file);
-        } else {
+        } catch (\Exception $exception) {
             return $this->json([
                 'message' => 'Not found'
             ], 404);
         }
-//        var_dump($dumpService);
-//        exit;
-//
-//
-////        var_dump($keys);
-//        exit;
-//        return $this->json([
-//            'message' => 'Welcome to your new controller!',
-//            'path' => 'src/Controller/DownloadController.php',
-//        ]);
+
+        $decryptedData = json_decode($decryptedData, true);
+        $file = $this->dumpService->getDumpFileByUuid($decryptedData['dumpuuid']);
+        if ($file === null) {
+            return $this->json([
+                'message' => 'Not found'
+            ], 404);
+        } else {
+            return $this->file($file);
+        }
     }
 }
